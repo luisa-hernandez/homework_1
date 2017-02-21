3679aa0f2ed730c83eb45d5f0ae9b54ea862b217
Hobo Noir <hobonetics@gmail.com>
2017-02-21 17:25:26 -0500

    added example of serialization

---
+++ Homework_1/TestSerialization.java
@@ -0,0 +1,55 @@
+package Homework_1;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class TestSerialization {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// create some strings
+		ArrayList<String> strings = new ArrayList<String>();
+
+		String str = "abcd";
+		for (int x = 0; x < 5; x++) {
+			strings.add(new String(str));
+			str = new StringBuilder(str).reverse().toString();
+		}
+		
+		try {
+			FileOutputStream fileOut = new FileOutputStream("/Users/Zorro/arena/jstrings");
+			ObjectOutputStream out = new ObjectOutputStream(fileOut);
+			out.writeObject(strings);
+			out.close();
+			fileOut.close();
+		} catch (IOException i) {
+			i.printStackTrace();
+		}
+		
+		ArrayList<String> istrings;
+		try {
+	         FileInputStream fileIn = new FileInputStream("/Users/Zorro/arena/jstrings");
+	         ObjectInputStream in = new ObjectInputStream(fileIn);
+	         istrings = (ArrayList<String>) in.readObject();
+	         in.close();
+	         fileIn.close();
+	      }catch(IOException i) {
+	         i.printStackTrace();
+	         return;
+	      }catch(ClassNotFoundException c) {
+	         c.printStackTrace();
+	         return;
+	      }
+
+		// print objects
+		for(String istr: istrings){
+			System.out.println(istr);
+		}
+	}
+
+}
